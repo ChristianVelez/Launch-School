6de959cd0f516970d9209cf8851ff5952ebc57f3
Christian Velez <christian.velez1@gmail.com>
2019-04-19 07:04:39 -0400

    Changes to loan calculator

--- LS_RB101/Lesson2/loan_calculator/loan_calculator.rb
+++ LS_RB101/Lesson2/loan_calculator/loan_calculator.rb
@@ -12,40 +12,42 @@ def valid_float?(num)
   (num.to_f.to_s.to_i == num) && (num.to_f > 1)
 end
 
+def final_validation(num)
+  valid_number?(num) || valid_float?(num)
+end
+
 def greeting
   prompt(MESSAGES['welcome'])
 end
 
 def input_order
-  ["loan amount", "interest rate", "loan duration in years"]
+  ['loan amount', 'interest rate', 'loan duration in years']
 end
 
-def grab_and_validate_input(input_order)
+def valid_input(input_order)
   input = ''
   loop do
     prompt("Please enter the #{input_order}")
     input = gets.chomp
-    if input.empty?
-      prompt(MESSAGES['empty_input'])
-    elsif valid_number?(input) || valid_float?(input)
-      break
-    else
-      prompt(MESSAGES['invalid_input'])
-    end
+    final_validation(input) ? break : prompt(MESSAGES['invalid_input'])
   end
   input
 end
 
 def format_numbers
-  @amount = grab_and_validate_input(input_order[0])
+  @amount = valid_input(input_order[0])
+  @interest = valid_input(input_order[1])
+  @duration = valid_input(input_order[2])
+end
+
+def grab_input
   @loan_amount = @amount.to_f
-  @interest = grab_and_validate_input(input_order[1])
   @interest_rate = (@interest.to_f / 100) / 12
-  @duration = grab_and_validate_input(input_order[2])
   @loan_duration = @duration.to_f * 12
 end
 
 def perform_operation
+  grab_input
   @monthly_payment = @loan_amount *
                      (@interest_rate /
                      (1 - (1 + @interest_rate)**-@loan_duration))
@@ -60,7 +62,7 @@ def another_operation?
   prompt(MESSAGES['another_calculation'])
   answer = gets.chomp.downcase
 
-  answer.start_with?("y") ? display_info : prompt(MESSAGES['goodbye'])
+  answer.start_with?('y') ? display_info : prompt(MESSAGES['goodbye'])
 end
 
 def display_info
